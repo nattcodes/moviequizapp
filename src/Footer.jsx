0caa89f2d1195e906fa49fb4d7a43d9e80a9b7b9
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-09-28 04:42:23 +0100

    d

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer class="footer">
-        <p>Copyright &copy; 2022 Kome Nath</p>
+        <p>Copyright &copy; 2023 Kome Nath</p>
     </footer>
   )
 }
